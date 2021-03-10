52d9c33808442ad5b48f24703362f60f17a90b02
AgentDS <zszxlsq@gmail.com>
2021-03-10 19:41:10 +0800

    ..

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,10 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
