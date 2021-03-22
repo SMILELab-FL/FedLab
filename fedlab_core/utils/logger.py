157817cc9bf674cfe981e4cc0ec94993b34d8f4e
zengdun <928255708@qq.com>
2021-03-22 23:13:09 +0800

    logger

--- fedlab_core/utils/logger.py
+++ fedlab_core/utils/logger.py
@@ -2,10 +2,21 @@ import logging
 
 
 class logger(object):
-    """record cmd info to file"""
-
+    """record cmd info to file and print it to cmd at the same time"""
     def __init__(self, log_file):
+        self.logger = logging.getLogger(self.args.username)
+        handler = logging.FileHandler(log_file, mode='w')
+        handler.setLevel(level=logging.INFO)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def info(self, log_str):
+        self.logger.info(log_str)
+
+    def error(self, error_str, log_str):
         raise NotImplementedError()
 
-    def log(self, log_str):
+    def warning(self, warning_str):
         raise NotImplementedError()
