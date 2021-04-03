32b1644a007f4632fc195d0550d012bd2288dc4a
zengdun <928255708@qq.com>
2021-04-03 16:29:51 +0800

    working on server.topology

--- fedlab_core/server/topology.py
+++ fedlab_core/server/topology.py
@@ -16,11 +16,16 @@ class EndTop(Process):
     Abstract class for server network topology
 
     If you want to define your own topology agreements, please overide this class.
+
+    args:
+        handler: back-end of server
+        server_address: (ip,port) to init `torch`
+        dist_backend: parameter to init `torch.distributed`
     """
 
     def __init__(self, handler, server_address, dist_backend):
-        self._server_handler = handler
-        self.server_addr = server_address  # ip:port
+        self._handler = handler
+        self.server_address = server_address  # ip:port
         self.dist_backend = dist_backend
 
     def run(self):
@@ -39,59 +44,72 @@ class EndTop(Process):
 class PipeTop(Process):
     """
     Abstract class for server Pipe topology
+    simple example
     """
 
-    def __init__(self, model, client_num, server_dist_info, client_dist_info):
+    def __init__(self, model, server_dist_info, client_dist_info):
 
-        self.connect_clients_process = None
-        self.connect_server_process = None
+        self._model = model
+
+        self.server_dist_info = server_dist_info
+        self.client_dist_info = client_dist_info
 
         # 临界资源
         # 子进程同步
 
     def run(self):
         """process function"""
-
-        self.connect_clients_process.start()
-        self.connect_server_process.start()
-
-        self.connect_clients_process.join()
-        self.connect_server_process.join()
+        raise NotImplementedError()
 
 
 class ConnectClient(EndTop):
     """Provide service to clients as a middle server"""
 
-    def __init__(self, locks, local_addr, world_size, rank=0, dist_backend='gloo'):
+    def __init__(self, handler, server_address, world_size, dist_backend='gloo'):
 
-        self.locks = locks
+        super(ConnectClient, self).__init__(
+            handler, server_address, dist_backend)
 
-        self.dist_backend = dist_backend
-        self.local_addr = local_addr
-        self.word_size = world_size
-        self.rank = rank
-        self.dist_backend = dist_backend
+        #self.locks = locks
 
     def run(self):
         """ """
         dist.init_process_group(backend=self.dist_backend, init_method='tcp://{}:{}'
-                                .format(self.local_addr[0], self.local_addr[1]),
-                                rank=self.rank, world_size=self.world_size)
-    
+                                .format(self.server_address[0], self.server_address[1]),
+                                rank=0, world_size=self._handler.client_num+1)
+
     def activate_clients(self):
-        return super().activate_clients()
+        """activate some of clients to join this FL round"""
+        usr_list = self._handler.select_clients()
+        payload = self._handler.buffer
+
+        for index in usr_list:
+            send_message(MessageCode.ParameterUpdate, payload, dst=index)
 
     def listen_clients(self):
-        return super().listen_clients()
+        """listen messages from clients"""
+        self._handler.start_round()  # flip the update_flag
+        while (True):
+            recv_message(self.buff)
+            sender = int(self.buff[0].item())
+            message_code = MessageCode(self.buff[1].item())
+            parameter = self.buff[2:]
+
+            self._handler.receive(sender, message_code, parameter)
+
+            if self._handler.update_flag:
+                # server_handler will turn this flag to True when model parameters updated
+                self._LOGGER.info("updated quit listen")
+                break
 
 
 class ConnectServer(ClientCommunicationTopology):
     """connect to upper server"""
 
-    def __init__(self, locks, server_addr, world_size, rank, dist_backend='gloo'):
+    def __init__(self, locks, server_address, world_size, rank, dist_backend='gloo'):
         # connect server
         dist.init_process_group(backend=dist_backend, init_method='tcp://{}:{}'
-                                .format(server_addr[0], server_addr[1]),
+                                .format(server_address[0], server_address[1]),
                                 rank=rank, world_size=world_size)
 
     def run(self):
@@ -112,7 +130,7 @@ class ServerSyncTop(EndTop):
 
     Args:
         ParameterServerHandler: a class derived from ParameterServerHandler
-        server_addr: (ip:port) ipadress for `torch.distributed` initialization, because this is a server, rank is set by 0.
+        server_address: (ip:port) ipadress for `torch.distributed` initialization, because this is a server, rank is set by 0.
         dist_backend: backend of `torch.distributed` (gloo, mpi and ncll) and gloo is default
         logger_file: path to the log file for this class
         logger_name: class name to initialize logger
@@ -127,7 +145,7 @@ class ServerSyncTop(EndTop):
         super(ServerSyncTop, self).__init__(handler=ParameterServerHandler,
                                             server_address=server_address, dist_backend=dist_backend)
         self.buff = torch.zeros(
-            self._server_handler.buffer.numel() + 2).cpu()  # 通信信息缓存 模型参数+2个控制参数位
+            self._handler.buffer.numel() + 2).cpu()  # 通信信息缓存 模型参数+2个控制参数位
 
         self._LOGGER = logger(logger_path, logger_name)
         self._LOGGER.info("Server initailize with ip address {} and distributed backend {}".format(
@@ -138,8 +156,8 @@ class ServerSyncTop(EndTop):
         self._LOGGER.info(
             "Waiting for the connection request from clients")
         dist.init_process_group(backend=self.dist_backend, init_method='tcp://{}:{}'
-                                .format(self.server_addr[0], self.server_addr[1]),
-                                rank=0, world_size=self._server_handler.client_num + 1)
+                                .format(self.server_address[0], self.server_address[1]),
+                                rank=0, world_size=self._handler.client_num + 1)
         self._LOGGER.info("Connect to client successfully")
 
         self.running = 3
@@ -157,14 +175,14 @@ class ServerSyncTop(EndTop):
             act_clients.join()
             wait_info.join()
 
-        for index in range(self._server_handler.client_num):
+        for index in range(self._handler.client_num):
             end_message = torch.Tensor([0])
             send_message(MessageCode.Exit, payload=end_message, dst=index)
 
     def activate_clients(self):
         """activate some of clients to join this FL round"""
-        usr_list = self._server_handler.select_clients()
-        payload = self._server_handler.buffer
+        usr_list = self._handler.select_clients()
+        payload = self._handler.buffer
 
         self._LOGGER.info(
             "client id list for this FL round: {}".format(usr_list))
@@ -173,16 +191,16 @@ class ServerSyncTop(EndTop):
 
     def listen_clients(self):
         """listen messages from clients"""
-        self._server_handler.start_round()  # flip the update_flag
+        self._handler.start_round()  # flip the update_flag
         while (True):
             recv_message(self.buff)
             sender = int(self.buff[0].item())
             message_code = MessageCode(self.buff[1].item())
             parameter = self.buff[2:]
 
-            self._server_handler.receive(sender, message_code, parameter)
+            self._handler.receive(sender, message_code, parameter)
 
-            if self._server_handler.update_flag:
+            if self._handler.update_flag:
                 # server_handler will turn this flag to True when model parameters updated
                 self._LOGGER.info("updated quit listen")
                 break
