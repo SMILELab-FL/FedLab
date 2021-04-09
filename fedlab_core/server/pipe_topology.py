12925dd3ac83f8a9e1fbd4ed32fb3ec602217a77
zengdun <928255708@qq.com>
2021-04-09 17:35:02 +0800

    pipe top design

--- fedlab_core/server/pipe_topology.py
+++ fedlab_core/server/pipe_topology.py
@@ -1,99 +1,49 @@
+from multiprocessing import Lock
 import torch.distributed as dist
 from torch.multiprocessing import Process
 
 from fedlab_core.utils.messaging import MessageCode, recv_message, send_message
 
 from fedlab_core.client.topology import ClientCommunicationTopology
-from fedlab_core.server.topology import EndTop
+from fedlab_core.server.handler import SyncParameterServerHandler
+from fedlab_core.server.topology import EndTop, ServerBasicTop
 
 
-class PipeTop(Process):
+class PipeTop(ClientCommunicationTopology):
     """
     Abstract class for server Pipe topology
     simple example
+
+
+    向上屏蔽局部的rank （通过全局rank到局部rank的映射解决）
+
+
+    双进程架构：
+        server进程，用于管理本地组的子FL系统
+        Pipe主进程，做消息通信和中继
+
+    难点：进程间通信和同步
+        考虑Queue 队列消息传递同步
+
     """
 
-    def __init__(self, model, server_process, client_process):
+    def __init__(self, model, args):
 
         self._model = model
-
-        self.sp = server_process
-        self.cp = client_process
-
-        # 临界资源
-        # 子进程同步
+        self._model.share_memory_()
 
     def run(self):
         """process function"""
-        
-        self.sp.start()
-        self.cp.start()
-
-        self.sp.join()
-        self.cp.join()
-
-
-class ConnectClient(EndTop):
-    """Provide service to clients as a middle server"""
-
-    def __init__(self, handler, server_address, world_size, dist_backend='gloo'):
-
-        super(ConnectClient, self).__init__(
-            handler, server_address, dist_backend)
-
-        #self.locks = locks
-
-    def run(self):
-        """ """
-        dist.init_process_group(backend=self.dist_backend, init_method='tcp://{}:{}'
-                                .format(self.server_address[0], self.server_address[1]),
-                                rank=0, world_size=self._handler.client_num+1)
-
-    def activate_clients(self):
-        """activate some of clients to join this FL round"""
-        usr_list = self._handler.select_clients()
-        payload = self._handler.buffer
-
-        for index in usr_list:
-            send_message(MessageCode.ParameterUpdate, payload, dst=index)
-
-    def listen_clients(self):
-        """listen messages from clients"""
-        self._handler.start_round()  # flip the update_flag
-        while (True):
-            recv_message(self.buff)
-            sender = int(self.buff[0].item())
-            message_code = MessageCode(self.buff[1].item())
-            parameter = self.buff[2:]
-
-            self._handler.receive(sender, message_code, parameter)
-
-            if self._handler.update_flag:
-                # server_handler will turn this flag to True when model parameters updated
-                self._LOGGER.info("updated quit listen")
-                break
-    
-    def share_buffer(self, buffer, lock):
         raise NotImplementedError()
 
-
-class ConnectServer(ClientCommunicationTopology):
-    """connect to upper server"""
-
-    def __init__(self, locks, server_address, world_size, rank, dist_backend='gloo'):
-        # connect server
-        dist.init_process_group(backend=dist_backend, init_method='tcp://{}:{}'
-                                .format(server_address[0], server_address[1]),
-                                rank=rank, world_size=world_size)
-
-    def run(self):
-        raise NotImplementedError()
-
-    def receive(self, sender, message_code, payload):
+    def on_receive(self, sender, message_code, payload):
+        """
+        接收上层server的激活信息
+        """
         return super().receive(sender, message_code, payload)
 
     def synchronise(self, payload):
+        """
+        将本组内的局部合并后的模型上传上层服务器
+        """
         return super().synchronise(payload)
-
-    def share_buffer(self, buffer, lock):
-        raise NotImplementedError()
