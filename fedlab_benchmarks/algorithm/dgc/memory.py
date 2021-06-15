b85d67979573ebf0b1dea09a08d815eb4f06a5a4
zengdun <928255708@qq.com>
2021-06-15 17:18:42 +0800

    fedlab_core/comunicator/compressor

--- fedlab_benchmarks/algorithm/dgc/memory.py
+++ fedlab_benchmarks/algorithm/dgc/memory.py
@@ -1,4 +1,5 @@
 import torch
+
 __all__ = ['Memory', 'DGCSGDMemory']
 
 
@@ -11,7 +12,7 @@ class Memory:
     @staticmethod
     def compensate(tensor, *args, **kwargs):
         return tensor
-    
+
     @staticmethod
     def update(*args, **kwargs):
         pass
@@ -19,7 +20,7 @@ class Memory:
     @staticmethod
     def state_dict():
         return None
-    
+
     @staticmethod
     def load_state_dict(state_dict):
         pass
@@ -27,8 +28,11 @@ class Memory:
 
 class DGCSGDMemory(Memory):
     """ Memory for momentum correction in DGC for momentum SGD optimizer"""
-    def __init__(self, momentum=0.9, nesterov=False,
-                 gradient_clipping=None, momentum_masking=True):
+    def __init__(self,
+                 momentum=0.9,
+                 nesterov=False,
+                 gradient_clipping=None,
+                 momentum_masking=True):
         self.gradient_clipping = gradient_clipping
         self.momentum_masking = momentum_masking
 
@@ -36,7 +40,7 @@ class DGCSGDMemory(Memory):
         self.nesterov = nesterov
         self.momentums = {}
         self.velocities = {}
-    
+
     def initialize(self, named_parameters):
         """
         if hvd.rank() == 0:
@@ -72,7 +76,9 @@ class DGCSGDMemory(Memory):
         """Update the momentums."""
         indices = ctx[0]
         if self.momentum_masking:
-            self.momentums[name].view(-1).index_fill_(0, indices, 0) # index_fill_(dim,index,val)按照参数index总的索引数确定的顺序，将原tensor用参数val值填充
+            self.momentums[name].view(-1).index_fill_(
+                0, indices, 0
+            )  # index_fill_(dim,index,val)按照参数index总的索引数确定的顺序，将原tensor用参数val值填充
         self.velocities[name].view(-1).index_fill_(0, indices, 0)
 
     def state_dict(self):
