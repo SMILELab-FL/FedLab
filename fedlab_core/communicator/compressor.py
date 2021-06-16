094d20b7e8531ffde81cb642d75049852eb6f209
zengdun <928255708@qq.com>
2021-06-16 15:06:02 +0800

    test topkcompressor

--- fedlab_core/communicator/compressor.py
+++ fedlab_core/communicator/compressor.py
@@ -1,10 +1,6 @@
-# 参考dgc中DGCCompressor的实现形式
 from abc import ABC, abstractmethod
 import math
-import random
 import torch
-from torch._C import DoubleTensor
-
 
 class Compressor(ABC):
     def __init__(self) -> None:
@@ -31,16 +27,15 @@ class TopkCompressor(Compressor):
 
         self.compress_ratio = compress_ratio if compress_ratio <= 1.0 else 1.0 / compress_ratio
 
-        self.attributes = {}  # 定义压缩矩阵的基本信息： [numel, shape, num_selects, num_samples, top_k_samples, sample_stride]
+        self.attributes = {}  # 定义压缩矩阵的基本信息： [numel, shape, top_k_samples]
 
     def initialize(self, named_parameters):
         for name, param in named_parameters:
             if torch.is_tensor(param):
                 numel = param.numel()
-                shape = list(param.size())
+                shape = param.shape
             else:
-                assert isinstance(param, (list, tuple))
-                numel, shape = param[0], param[1]
+                raise ValueError("invalid type")
 
             top_k_samples = int(math.ceil(numel * self.compress_ratio))
             self.attributes[name] = (numel, shape, top_k_samples)
@@ -48,11 +43,12 @@ class TopkCompressor(Compressor):
     def compress(self, tensor, name):
         # 对于已注册的数据结构/模型参数压缩
         if self.compress_ratio < 1.0 and name in self.attributes:
-            tensor = tensor.view(-1)
-        
-            numel, shape, top_k_samples = self.attributes[name]
 
+            tensor = tensor.view(-1)
             importance = tensor.abs()
+
+            numel, shape, top_k_samples = self.attributes[name]
+            
             _, indices = torch.topk(importance,
                                     top_k_samples,
                                     0,
@@ -66,11 +62,10 @@ class TopkCompressor(Compressor):
     def decompress(self, tensor, ctx):
         name, shape = ctx
         if self.compress_ratio < 1.0 and name in self.attributes:
+            
             values, indices = tensor
-
-            de_tensor = torch.zeros(size=shape).index_put_([indices],
-                                                           values,
-                                                           accumulate=True)
+            de_tensor = torch.zeros(size=shape).view(-1)
+            de_tensor = de_tensor.index_put_([indices], values, accumulate=True).view(shape)
             return de_tensor
         else:
             raise ValueError("invalid value")
