575946c7884e5d73e62bc3bdd146000b091a8d15
zengdun <928255708@qq.com>
2021-07-17 19:49:19 +0800

    save for now

--- fedlab_benchmarks/algorithm/fedavg/standalone_fedavg.py
+++ fedlab_benchmarks/algorithm/fedavg/standalone_fedavg.py
@@ -18,10 +18,6 @@ from fedlab_utils.aggregator import Aggregators
 from fedlab_utils.models.lenet import LeNet
 from fedlab_core.client.serial_trainer import SerialTrainer
 
-
-
-
-
 # configuration
 # server config
 parser = argparse.ArgumentParser(description='Distbelief training example')
@@ -36,35 +32,36 @@ parser.add_argument('--lr', type=float, default=0.1)
 
 # cuda config
 parser.add_argument('--gpu', type=str, default=0)
-
-
 parser.add_argument('--wandb', type=bool, default=False)
 
 args = parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"] = str(args.cuda)
 
-
 model = LeNet().cuda()
 aggregator = Aggregators.fedavg_aggregate
 criterion = torch.nn.CrossEntropyLoss()
 
+# get raw dataset
+# test
 trainset = torchvision.datasets.MNIST(
     root='/home/zengdun/datasets/mnist/', train=True, download=True, transform=transforms.ToTensor())
-
 testset = torchvision.datasets.MNIST(
     root='/home/zengdun/datasets/mnist/', train=False, download=True, transform=transforms.ToTensor())
 
 test_loader = torch.utils.data.DataLoader(
     testset, batch_size=len(testset), drop_last=False, shuffle=False)
 
+# partion dataset for clients
 total_client_num = args.total_client  # client总数
 data_indices = noniid_slicing(
     trainset, num_clients=args.total_client, num_shards=200)
 
+
 local_model = deepcopy(model)
 criterion = nn.CrossEntropyLoss()
 
+# use fedlab modeles
 serial_handler = SerialTrainer(
     model=local_model, dataset=trainset, data_slices=data_indices, aggregator=aggregator)
 model_params = SerializationTool.serialize_model(model)
