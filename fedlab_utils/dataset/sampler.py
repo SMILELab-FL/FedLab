ad41fdfcaa0df3e84a44fe7bf0ebfd7b7ae2bf5c
zengdun <928255708@qq.com>
2021-08-03 14:30:12 +0800

    update sampler

--- fedlab_utils/dataset/sampler.py
+++ fedlab_utils/dataset/sampler.py
@@ -64,37 +64,44 @@ class FedDistributedSampler(torch.utils.data.Sampler):
             replicas. If ``False``, the sampler will add extra indices to make
             the data evenly divisible across the replicas. Default: ``False``.
     """
-    def __init__(self,
-                 dataset,
-                 num_replicas=None,
-                 client_id=None,
-                 shuffle=True):
-
+    def __init__(self, dataset, num_replicas=None, rank=None, shuffle=True):
         if num_replicas is None:
             if not dist.is_available():
-                raise RuntimeError(
-                    "Requires distributed package to be available")
+                raise RuntimeError("Requires distributed package to be available")
             num_replicas = dist.get_world_size() - 1
-
-        if client_id is None:
+        if rank is None:
             if not dist.is_available():
-                raise RuntimeError(
-                    "Requires distributed package to be available")
+                raise RuntimeError("Requires distributed package to be available")
             rank = dist.get_rank() - 1
-        
-        if rank >= num_replicas or rank < 0:
-            raise ValueError("Invalid rank {}, rank should be in the interval"
-                             " [0, {}]".format(rank, num_replicas - 1))
-
-        self._dataset = dataset
-
+        self.dataset = dataset
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.epoch = 0
+        self.num_samples = int(math.ceil(len(self.dataset) * 1.0 / self.num_replicas))
         self.total_size = self.num_samples * self.num_replicas
         self.shuffle = shuffle
 
     def __iter__(self):
-        indices = None
+        # deterministically shuffle based on epoch
+        g = torch.Generator()
+        g.manual_seed(self.epoch)
+        if self.shuffle:
+            indices = torch.randperm(len(self.dataset), generator=g).tolist()
+        else:
+            indices = list(range(len(self.dataset)))
+
+        # add extra samples to make it evenly divisible
+        indices += indices[:(self.total_size - len(indices))]
+        assert len(indices) == self.total_size
+
+        # subsample
+        indices = indices[self.rank:self.total_size:self.num_replicas]
+        assert len(indices) == self.num_samples
+
         return iter(indices)
 
     def __len__(self):
         return self.num_samples
 
+    def set_epoch(self, epoch):
+        self.epoch = epoch
