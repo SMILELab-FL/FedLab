dc52fc1c67236148eb111dfd114f63080c86291e
zengdun <928255708@qq.com>
2021-08-23 20:50:13 +0800

    update scale modules

--- fedlab_benchmarks/algorithm/fedavg/scale/client.py
+++ fedlab_benchmarks/algorithm/fedavg/scale/client.py
@@ -1,32 +1,107 @@
-from logging import log
 import torch
 import argparse
 import sys
 import os
 
+import torchvision
+import torchvision.transforms as transforms
+
 sys.path.append("../../../../")
 
-from torch import nn
 from fedlab.core.network_manager import NetworkManager
-from fedlab.core.client.trainer import ClientSGDTrainer
+from fedlab.core.client.trainer import SerialTrainer
 from fedlab.core.network import DistNetwork
-from fedlab.utils.logger import Logger
 
 from fedlab.core.communicator.package import Package
 from fedlab.core.communicator.processor import PackageProcessor
 
-from setting import get_model, get_dataset
+from fedlab.utils.logger import Logger
+from fedlab.utils.aggregator import Aggregators
+from fedlab.utils.dataset.slicing import noniid_slicing, random_slicing
+from fedlab.utils.message_code import MessageCode
+
+from setting import get_model
 
 
 class ScaleClientManager(NetworkManager):
-    def __init__(self, handler, network, logger):
-        super().__init__(handler, network, logger=logger)
+    def __init__(self, handler, network):
+        super().__init__(network, handler)
 
     def setup(self):
         super().setup()
-        content = torch.Tensor(self._handler.client_num)
+        content = torch.Tensor([self._handler.client_num]).int()
         setup_pack = Package(content=content, data_type=1)
         PackageProcessor.send_package(setup_pack, dst=0)
 
     def on_receive(self, sender_rank, message_code, payload):
-        return super().on_receive(sender_rank, message_code, payload)
+        if message_code == MessageCode.ParameterUpdate:
+            model_parameters = payload[0]
+            sender_rank, message_code, payload = PackageProcessor.recv_package(src=0)
+            print(payload)
+
+    def run(self):
+        self.setup()
+
+        sender_rank, message_code, payload = PackageProcessor.recv_package(src=0)
+        self.on_receive(sender_rank, message_code, payload)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Distbelief training example")
+
+    parser.add_argument("--ip", type=str, default="127.0.0.1")
+    parser.add_argument("--port", type=str, default="3002")
+    parser.add_argument("--world_size", type=int)
+    parser.add_argument("--rank", type=int)
+    parser.add_argument("--num", type=int)
+
+    parser.add_argument("--dataset", type=str, default="mnist")
+    parser.add_argument("--partition", type=str, default="iid")
+    parser.add_argument("--total_client", type=int, default=10)
+
+    parser.add_argument("--gpu", type=str, default="0,1,2,3")
+    parser.add_argument("--ethernet", type=str, default=None)
+
+    args = parser.parse_args()
+
+    if args.gpu != "-1":
+        args.cuda = True
+        os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+    else:
+        args.cuda = False
+
+    root = "../../../../../datasets/mnist/"
+    trainset = torchvision.datasets.MNIST(root=root,
+                                          train=True,
+                                          download=True,
+                                          transform=transforms.ToTensor())
+
+    if args.partition == "noniid":
+        data_indices = noniid_slicing(trainset,
+                                      num_clients=args.total_client,
+                                      num_shards=200)
+    elif args.partition == "iid":
+        data_indices = random_slicing(trainset, num_clients=args.total_client)
+    else:
+        raise ValueError("invalid partition type ", args.partition)
+
+    model = get_model(args)
+    aggregator = Aggregators.fedavg_aggregate
+
+    total_client_num = args.total_client  # client总数
+    network = DistNetwork(address=(args.ip, args.port),
+                          world_size=args.world_size,
+                          rank=args.rank,
+                          ethernet=args.ethernet)
+
+    LOGGER = Logger(log_name="client " + str(args.rank))
+
+    trainer = SerialTrainer(model=model,
+                            dataset=trainset,
+                            data_slices=data_indices,
+                            aggregator=aggregator,
+                            args=args)
+
+    manager_ = ScaleClientManager(handler=trainer, network=network)
+
+    manager_.run()
--- fedlab_benchmarks/algorithm/fedavg/scale/server.py
+++ fedlab_benchmarks/algorithm/fedavg/scale/server.py
@@ -1,26 +1,32 @@
-from fedlab.core.communicator.processor import PackageProcessor
-from fedlab.core.communicator.package import Package
 import os
+import random
 import sys
 import argparse
 
-sys.path.append('../../../../')
-from fedlab.utils.logger import Logger
-from fedlab.core.server.handler import SyncParameterServerHandler
-from fedlab.core.network_manager import NetworkManager
-from fedlab.core.network import DistNetwork
-from fedlab.core.coordinator import coordinator
-from setting import get_model
 import torch
-from setting import get_dataset
 import torchvision
 from torchvision import transforms
+
+sys.path.append('../../../../')
+
+from fedlab.core.server.handler import SyncParameterServerHandler, ParameterServerBackendHandler
+from fedlab.core.server.manager import ServerSynchronousManager
+from fedlab.core.network_manager import NetworkManager
+from fedlab.core.network import DistNetwork
+from fedlab.core.coordinator import Coordinator
+from fedlab.core.communicator.processor import PackageProcessor
+from fedlab.core.communicator.package import Package
+
 from fedlab.utils.functional import evaluate
+from fedlab.utils.logger import Logger
+from fedlab.utils.message_code import MessageCode
+
+from setting import get_model, get_dataset
 
 
-class ScaleSynchronousServer(NetworkManager):
-    def __init__(self, handler, network, logger):
-        super().__init__(handler, network, logger=logger)
+class ScaleSynchronousServer(ServerSynchronousManager):
+    def __init__(self, network, handler):
+        super().__init__(network=network, handler=handler)
 
     def setup(self):
         super().setup()
@@ -29,48 +35,65 @@ class ScaleSynchronousServer(NetworkManager):
             _, _, content = PackageProcessor.recv_package(src=rank)
             data = content[0].item()
             map[rank] = data
-        self.coordinator = coordinator(map)
+        self.coordinator = Coordinator(map)
+        self._handler.client_num_in_total = int(
+            sum(self.coordinator.map.values()))
+        self._handler.client_num_per_round = max(
+            1, int(self._handler.sample_ratio * self._handler.client_num_in_total))
 
     def run(self):
         self.setup()
+        self.activate_clients()
 
     def activate_clients(self):
         clients_this_round = self._handler.sample_clients()
+        print(clients_this_round)
         rank_dict = self.coordinator.map_id_list(clients_this_round)
-        for key, values in rank_dict:
-            pass
 
+        print(rank_dict)
+
+        for rank, values in rank_dict.items():
+
+            param_pack = Package(message_code=MessageCode.ParameterUpdate,
+                                 content=self._handler.model_parameters)
+            PackageProcessor.send_package(package=param_pack, dst=rank)
+
+            id_lis = torch.Tensor(values).int()
+            print(id_lis)
+            act_pack = Package(message_code=MessageCode.ParameterUpdate,
+                               content=id_lis, data_type=1)
+            PackageProcessor.send_package(package=act_pack, dst=rank)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='FL server example')
 
-    parser.add_argument('--ip', type=str)
-    parser.add_argument('--port', type=str)
+    parser.add_argument('--ip', type=str, default="127.0.0.1")
+    parser.add_argument('--port', type=str, default="3002")
     parser.add_argument('--world_size', type=int)
 
     parser.add_argument('--round', type=int, default=5)
-    parser.add_argument('--dataset', type=str)
+    parser.add_argument('--dataset', type=str, default="mnist")
     parser.add_argument('--ethernet', type=str, default=None)
     parser.add_argument('--sample', type=float, default=1)
 
     args = parser.parse_args()
 
     model = get_model(args)
+
     LOGGER = Logger(log_name="server")
+
     handler = SyncParameterServerHandler(model,
-                                         client_num_in_total=args.world_size -
-                                         1,
+                                         client_num_in_total=1,
                                          global_round=args.round,
                                          logger=LOGGER,
                                          sample_ratio=args.sample)
 
     network = DistNetwork(address=(args.ip, args.port),
                           world_size=args.world_size,
-                          rank=0,
-                          ethernet=args.ethernet)
+                          rank=0)
 
-    manager_ = ScaleSynchronousServer(handler=handler,
-                                      network=network,
-                                      logger=LOGGER)
+    LOGGER = Logger(log_name="server")
+
+    manager_ = ScaleSynchronousServer(network=network, handler=handler)
     manager_.run()
