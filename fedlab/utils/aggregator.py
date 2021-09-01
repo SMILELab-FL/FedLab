95bc181e7d7d4729b89313359367c98010f9fd97
AgentDS <zszxlsq@gmail.com>
2021-09-01 14:17:45 +0800

    add weights in fedavg_aggregator

--- fedlab/utils/aggregator.py
+++ fedlab/utils/aggregator.py
@@ -17,20 +17,31 @@ import torch
 
 class Aggregators(object):
     """Define the algorithm of parameters aggregation"""
+
     @staticmethod
-    def fedavg_aggregate(serialized_params_list):
+    def fedavg_aggregate(serialized_params_list, weights=None):
         """FedAvg aggregator
 
         Paper: http://proceedings.mlr.press/v54/mcmahan17a.html
 
         Args:
             serialized_params_list (list[torch.Tensor])): Merge all tensors following FedAvg.
+            weights (list, numpy.array or torch.Tensor, optional): Weights for each params, the length of weights need to be same as length of ``serialized_params_list``
 
         Returns:
             torch.Tensor
         """
-        serialized_parameters = torch.mean(torch.stack(serialized_params_list),
-                                           dim=0)
+        if weights is None:
+            weights = torch.ones(len(serialized_params_list))
+
+        if not isinstance(weights, torch.Tensor):
+            weights = torch.tensor(weights)
+
+        weights = weights / torch.sum(weights)
+        assert torch.all(weights >= 0), "weights should be non-negative values"
+        serialized_parameters = torch.sum(torch.stack(serialized_params_list, dim=-1) * weights,
+                                          dim=-1)
+
         return serialized_parameters
 
     @staticmethod
