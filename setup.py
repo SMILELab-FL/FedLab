e4e0757a06783f2da78fb3dff3997ee687c59966
zengdun <928255708@qq.com>
2021-09-01 16:42:06 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-# -*- coding: utf-8 -*-
-# @Time    : 3/19/21 4:13 PM
-# @Author  : Siqi Liang
-# @Contact : zszxlsq@gmail.com
-# @File    : setup.py
-# @Software: PyCharm
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+from os import path as os_path
+import fedlab
+
+this_directory = os_path.abspath(os_path.dirname(__file__))
 
 
 def get_readme():
@@ -18,38 +16,48 @@ def get_readme():
     return long_description
 
 
+def read_file(filename):
+    with open(os_path.join(this_directory, filename), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+
+def read_requirements(filename):
+    return [
+        line.strip() for line in read_file(filename).splitlines()
+        if not line.startswith('#')
+    ]
+
+
 setup(
-    name="FedLab",
-    version="1.0",
-    author="Dun Zeng, Siqi Liang, Xiangjing Hu, Congjian Luo",
-    author_email="zengdun.cs@gmail.com, zszxlsq@gmail.com",
+    name="fedlab",
+    version=fedlab.__version__,
+    keywords=["federated learning", "depp learning", "pytorch"],
+    author="Dun Zeng, Siqi Liang, Xiangjing Hu",
+    author_email=
+    "zengdun.cs@gmail.com, zszxlsq@gmail.com, starryhu@foxmail.com",
     maintainer="Dun Zeng, Siqi Liang, Xiangjing Hu",
-    maintainer_email="zengdun.cs@gmail.com, zszxlsq@gmail.com",
-    description="A flexible Federated Learning Framework based on PyTorch, simplifying your Federated Learning research.",
-    long_description=get_readme(),
+    maintainer_email=
+    "zengdun.cs@gmail.com, zszxlsq@gmail.com, starryhu@foxmail.com",
+    description=
+    "A flexible Federated Learning Framework based on PyTorch, simplifying your Federated Learning research.",
+    long_description=read_file('README.md'),
+    license="Apache-2.0 License",
     url="https://github.com/SMILELab-FL/FedLab",
-    packages=find_packages(exclude=['docs', 'tests']),  # TODO: add things in 'exclude'
-    install_requires=['torch>=1.7.1',
-                      'torchvision>=0.8.2',
-                      'numpy',
-                      'spacy',
-                      'pynvml',
-                      'pickle'],
+    #packages=find_packages(exclude=['fedlab_benchmarks', 'tests', 'docs']),
+    packages=find_packages(include=['fedlab','fedlab.*','LICENSE']),
+    install_requires=read_requirements('requirements.txt'),
     python_requires='>=3.6',
     classifiers=[
         'Intended Audience :: Research',
-
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
-
         'License :: OSI Approved :: Apache Software License',
-
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    test_suite="tests.get_tests"
-)
+    test_suite="tests.get_tests")
