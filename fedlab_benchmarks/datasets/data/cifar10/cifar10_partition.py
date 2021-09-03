3d14e1f5772680aca802d868b9fce41b0c1f0aff
zengdun <928255708@qq.com>
2021-09-03 13:01:04 +0800

    update dataset

---
+++ fedlab_benchmarks/datasets/data/cifar10/cifar10_partition.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.path.append("../../../../")
+from fedlab.utils.functional import save_dict
+from fedlab.utils.dataset.slicing import noniid_slicing, random_slicing
+
+import torchvision
+
+trainset = torchvision.datasets.CIFAR10(root="./", train=True, download=False)
+
+data_indices = noniid_slicing(trainset, num_clients=100, num_shards=200)
+save_dict(data_indices, "cifar10_noniid.pkl")
+
+data_indices = random_slicing(trainset, num_clients=100)
+save_dict(data_indices, "cifar10_iid.pkl")
