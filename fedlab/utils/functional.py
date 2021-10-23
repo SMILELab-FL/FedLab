ba59ea9d165350c17b69c637dc0e7abf323ed29b
AgentDS <zszxlsq@gmail.com>
2021-10-23 19:59:08 +0800

    add top-k acuracy function

--- fedlab/utils/functional.py
+++ fedlab/utils/functional.py
@@ -219,3 +219,19 @@ def partition_report(targets, data_indices, class_num=None, verbose=True, file=N
         fh = open(file, "w")
         fh.write("\n".join(reports))
         fh.close()
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the top-k accuracy for the specified values of k, in range of [0, 1]"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(1.0 / batch_size))
+    return res
