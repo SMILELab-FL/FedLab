3a11536de735a12a69430bc8cb17200bb26d69b5
AgentDS <zszxlsq@gmail.com>
2022-10-01 12:42:37 +0800

    try to finish PackageProcessor test

--- tests/test_core/test_communicator/test_processor.py
+++ tests/test_core/test_communicator/test_processor.py
@@ -30,52 +30,83 @@ import torch.distributed as dist
 from torch.multiprocessing import Process
 
 
-class test_sender(Process):
-    def __init__(self, content) -> None:
-        super(test_sender, self).__init__()
-        self.net = DistNetwork(address=("localhost", "3001"),
-                               world_size=2,
-                               rank=1)
-        self.tensor_list = content
+# class test_sender(Process):
+#     def __init__(self, content) -> None:
+#         super(test_sender, self).__init__()
+#         self.net = DistNetwork(address=("localhost", "3001"),
+#                                world_size=2,
+#                                rank=1)
+#         self.tensor_list = content
 
-    def run(self):
-        self.net.init_network_connection()
-        p = Package(message_code=MessageCode.ParameterUpdate,
-                    content=self.tensor_list)
-        PackageProcessor.send_package(p, dst=0)
-        self.net.close_network_connection()
+#     def run(self):
+#         self.net.init_network_connection()
+#         p = Package(message_code=MessageCode.ParameterUpdate,
+#                     content=self.tensor_list)
+#         PackageProcessor.send_package(p, dst=0)
+#         self.net.close_network_connection()
+
+# def sender_run(network, content):
+#     network.init_network_connection()
+#     p = Package(message_code=MessageCode.ParameterUpdate,
+#                 content=content)
+#     PackageProcessor.send_package(p, dst=0)
+#     network.close_network_connection()
+
+# def receiver_run(network, check_content):
+#     network.init_network_connection()
+#     _, _, content = PackageProcessor.recv_package(src=1)
+    
+
+# class test_receiver(Process):
+#     def __init__(self, check_content) -> None:
+#         super(test_receiver, self).__init__()
+#         self.net = DistNetwork(address=("localhost", "3001"),
+#                                world_size=2,
+#                                rank=0)
+#         self.check_list = check_content
+
+#     def run(self):
+#         self.net.init_network_connection()
+#         _, _, content = PackageProcessor.recv_package(src=1)
+
+#         for t, p_t in zip(content, self.check_list):
+#             assert torch.equal(t, p_t)
+
+#         self.net.close_network_connection()
 
 
-class test_receiver(Process):
-    def __init__(self, check_content) -> None:
-        super(test_receiver, self).__init__()
-        self.net = DistNetwork(address=("localhost", "3001"),
-                               world_size=2,
-                               rank=0)
-        self.check_list = check_content
-
-    def run(self):
-        self.net.init_network_connection()
-        _, _, content = PackageProcessor.recv_package(src=1)
-
-        for t, p_t in zip(content, self.check_list):
-            assert torch.equal(t, p_t)
-
-        self.net.close_network_connection()
-
-
-class ProcessorTestCase(unittest.TestCase):
+class PackageProcessorTestCase(unittest.TestCase):
     def setUp(self):
         tensor_num = 5
         tensor_sizes = [randint(3, 15) for _ in range(tensor_num)]
-        tensor_list = [torch.rand(size) for size in tensor_sizes]
-
-        self.p1 = test_sender(tensor_list)
-        self.p2 = test_receiver(tensor_list)
+        self.tensor_list = [torch.rand(size) for size in tensor_sizes]
 
     def test_send_and_receive(self):
-        self.p1.start()
-        self.p2.start()
+        send_network = DistNetwork(address=("localhost", "3001"),
+                                   world_size=2,
+                                   rank=1)
+        recv_network = DistNetwork(address=("localhost", "3001"),
+                                   world_size=2,
+                                   rank=0)
+        sender = Process(target=self._sender_run, args=(send_network, self.tensor_list))
+        receiver = Process(target=self._receiver_run, args=(recv_network, self.tensor_list))
+        
+        sender.start()
+        receiver.start()
 
-        self.p1.join()
-        self.p2.join()
+        sender.join()
+        receiver.join()
+
+    def _sender_run(self, send_network, content):
+        send_network.init_network_connection()
+        p = Package(message_code=MessageCode.ParameterUpdate,
+                    content=content)
+        PackageProcessor.send_package(p, dst=0)
+        send_network.close_network_connection()
+
+    def _receiver_run(self, recv_network, check_content):
+        recv_network.init_network_connection()
+        _, _, content = PackageProcessor.recv_package(src=1)
+        for t, p_t in zip(content, check_content):
+            self.assertTrue(torch.equal(t, p_t))
+        recv_network.close_network_connection()
